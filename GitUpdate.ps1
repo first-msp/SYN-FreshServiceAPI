ee049611f0bc1eeb8c36298f2070ff676d3c8fdd
Ashley Collinge <ashley.collinge@outlook.com>
2018-06-03 23:25:43 +0100

    pep8 and documentation update

---
+++ GitUpdate.ps1
@@ -0,0 +1,8 @@
+﻿Stop-Service -Name W3SVC
+Stop-ScheduledTask -TaskName "RunCeleryWorker"
+Stop-Process -Name celery
+Set-Location -Path C:\inetpub\wwwroot\
+Remove-Item -Recurse -Force .\SYN-FreshServiceAPI
+git clone https://github.com/ashleycollinge1/SYN-FreshServiceAPI
+Start-Service -Name W3SVC
+Start-ScheduledTask -TaskName "RunCeleryWorker"
